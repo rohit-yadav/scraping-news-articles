eaed7f355c3437376c1d00f9a7a34959848ca63e
Rohit Yadav <rohityadav2@outlook.com>
2018-07-01 12:34:47 +0530

    Extract useful sections

--- Scraping News Articles.py
+++ Scraping News Articles.py
@@ -62,3 +62,28 @@ def parse_html(to_parse):
     soup = bs4.BeautifulSoup(to_parse, 'html.parser')
     return soup
 
+
+# In[6]:
+
+
+# Function to collect all sections links
+# Like world news, national news ...
+
+def all_section(main_url):
+    """
+    It takes a main url of the newspaper and then
+    finds almost all the sections in the newspaper.
+    Finally, it returns the section which we will scrap.
+    """
+    soup = parse_html(request_url(url))
+    ul = soup.find("div", class_="MainLMenu tab").ul
+    section_list = []
+    for li in ul.find_all("li"):
+        section_list.append(li.a.get('href'))
+    # Remove the section which we will not consider
+    # Like the video section and others
+    remove = [0, 1, -1, -1, -1]
+    for i in remove:
+        section_list.remove(section_list[i])
+    return section_list
+
