50fce7daa86bcdb2144cc699b8cbab5d8a24773c
rohit <rohityadav2@outlook.com>
2018-09-01 11:50:41 +0530

    Split types of layout
    Page has two types of layout, so segregating the link will come handy

--- scraping_news_articles.py
+++ scraping_news_articles.py
@@ -105,3 +105,53 @@ section_urls
 # **Orientation of the section pages**
 # 
 # In this newspaper there are basically two types of layouts: Grid View and List View. So we will filter the section of the grid and list view seperately. As depending upon the layouts the html fromat cahges, therefore we will have to scrap them differently.
+
+# In[11]:
+
+
+# Keep url of the section which has Grid layout
+grid_layout_urls = [3, 4, 6, 7, 8, 9]
+
+# All Grid half urls
+grid_urls = []
+
+
+# In[12]:
+
+
+## Keep url of the section, which has linear layout(List) view
+list_layout_urls = [0, 1, 5]
+
+# All List half urls
+list_urls = []
+
+
+# In[13]:
+
+
+# Extracting only Gride View section page urls
+for filter_url in grid_layout_urls:
+    grid_urls.append(section_urls[filter_url])
+
+
+# In[14]:
+
+
+# Extracting only List View section page urls
+for filter_url in list_layout_urls:
+    list_urls.append(section_urls[filter_url])
+
+
+# In[15]:
+
+
+# Checking the Grid View urls
+grid_urls
+
+
+# In[16]:
+
+
+# Checking List view urls
+list_urls
+
